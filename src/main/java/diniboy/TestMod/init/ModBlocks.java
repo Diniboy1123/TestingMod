a642ab5419decad6d168a67e35d24b828efae1b0
Diniboy1123 <diniboy1123@users.noreply.github.com>
2017-06-02 20:20:09 +0200

    Fix imports

--- src/main/java/diniboy/TestMod/init/ModBlocks.java
+++ src/main/java/diniboy/TestMod/init/ModBlocks.java
@@ -5,6 +5,9 @@ import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
@@ -28,3 +31,4 @@ public class ModBlocks {
 	}
 
 }
+
